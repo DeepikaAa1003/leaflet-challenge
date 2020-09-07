42ef474a3608508eb73156661fcca6271556f303
unknown <deepika1003@yahoo.com>
2020-09-07 15:12:44 -0500

    added both fault and earthquake layers to level2.js

--- Leaflet-Step-1/static/js/level1.js
+++ Leaflet-Step-1/static/js/level1.js
@@ -1,27 +1,38 @@
 // code for creating Basic Map (Level 1)
-  
-  // Create a new map
-  const myMap = L.map("map", {
-    center:  [37.0902, -95.7129],
-    zoom: 3
-  });
 
-  
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-const darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "dark-v10",
-  accessToken: API_KEY
-}).addTo(myMap);
+function createMaps(earthquakeMarkers){
 
+
+        
+        // Create a new map
+        const myMap = L.map("map", {
+          center:  [46.7557, -136.6825],
+          zoom: 5        
+        });
+
+        
+      L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+          tileSize: 512,
+          maxZoom: 18,
+          zoomOffset: -1,
+          id: "mapbox/streets-v11",
+          accessToken: API_KEY
+        }).addTo(myMap);
+      const darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+      }).addTo(myMap);
+      
+      //Add markers to map
+      earthquakeMarkers.addTo(myMap);
+      //Add legend to map
+      createLegend(myMap);
+
+
+}
 // Store our API endpoint as queryUrl
  const geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson";
 
@@ -92,22 +103,22 @@ d3.json(geoData, function(data) {
       
  // console.log(jsonData.features);
   // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
-      L.geoJSON(data.features, {
+      earthquakeMarkers = L.geoJSON(data.features, {
         
         pointToLayer: pointToLayer,
         
         // popup for each marker
         onEachFeature:  addPopup
-        }).addTo(myMap);
-  
-        createLegend();
+        });
+        createMaps(earthquakeMarkers);
+        
 
   
   
 });
 
  
-function createLegend(){
+function createLegend(myMap){
 
           // creating the legend
           var legend = L.control({position: 'bottomright'});
--- Leaflet-Step-2/static/js/level2.js
+++ Leaflet-Step-2/static/js/level2.js
@@ -1,4 +1,91 @@
-// code for creating Advanced Map (Level 2)
+//Adding functions for earthquake layer
+
+function markerSize(magnitude) {
+
+  if(magnitude > 5.5) {
+    return magnitude * 11;
+  }
+  else if (magnitude > 5) {
+    return magnitude * 6;
+  }
+  else if (magnitude > 4){
+    return magnitude * 3;
+  }
+  else {
+  return magnitude * 0.5;
+  }
+
+  
+}
+
+// function to return the color based on magnitude
+function markerColor(Significance) {
+    if (Significance >= 1000){
+      return "#E24125"
+    }
+    else if (Significance >= 750){  
+      return "#E5612A"
+    }
+    else if ( Significance >= 500){
+      return "#E8812F"
+    }
+    else if( Significance >= 250){
+      return "#ECA035"
+    }
+    else {
+      return "#FFFFFF"
+    }
+}
+
+
+
+function pointToLayer(feature, location) {
+  var options = {
+    stroke: false,
+    color: "#000",
+    fillColor: markerColor(feature.properties.sig),
+    radius: markerSize(feature.properties.mag),
+    fillOpacity: 1,
+    weight: 1,
+    opacity: 1
+  }
+
+  return L.circleMarker(location, options);
+
+}
+   
+   
+  function addPopup(feature, layer) {
+  
+  // Give each feature a popup describing the place and time of the earthquake
+  return layer.bindPopup(`<h4> EarthQuake: ${feature.properties.place} </h4> <hr> <h4> Time: ${Date(feature.properties.time)} </h4> <hr> <h4>Magnitude: ${feature.properties.mag} </h4> <hr> <h4>Significance: ${feature.properties.sig} </h4> `);
+}
+
+function createLegend(myMap){
+
+  // creating the legend
+  var legend = L.control({position: 'bottomright'});
+
+  // add legend to map
+  legend.onAdd = function(myMap) {
+    var div = L.DomUtil.create("div", "info legend");
+    var magRange = [0,250,500,750,1000];
+    var colors = ["#FFFFFF", "#ECA035","#E8812F","#E5612A","#E24125"];
+    var labels = ['<strong> EQ Significance </strong> <br><br>'];
+ 
+    for (var i = 0; i < magRange.length; i++) {
+      labels.push(
+        "<i style='background: " + colors[i] + "'></i> " +
+        magRange[i] + (magRange[i + 1] ? "&ndash;" + magRange[i + 1] + "<br>" : "+"))
+        }  //ends for loop
+        div.innerHTML = labels.join('<br>');
+    
+    return div;
+    
+  }
+  legend.addTo(myMap);
+}
+//Add functions for Fault layer
 function selectFaultWeight(slipRate){
 
     if(slipRate === "Greater than 5.0 mm/yr"){
@@ -21,36 +108,55 @@ function selectFaultStyle(feature) {
 
 // Store our API endpoint as queryUrl
 const queryUrl = "static/data/qfaults_latest_quaternary.geojson";
+// Store our API endpoint as queryUrl
+const geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson";
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-//   console.log(data.features);
-  // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
-  FaultList = [];
-  data.features.map(feature => {
-        
-      if(feature.properties.slip_rate === "Greater than 5.0 mm/yr"){
-        FaultList.push(feature);
-      }else if (feature.properties.slip_rate === "Between 1.0 and 5.0 mm/yr"){
-          FaultList.push(feature);
-      }
-  });
-  
-  faultMarkers = L.geoJson(FaultList, {
-    // Style each feature (in this case a neighborhood)
-    style: selectFaultStyle,
-    // Called on each feature
-    onEachFeature: function(feature, layer) {
+
+//Read data for earthquake and fault lines
+
+d3.json(geoData, function(data) {
+  d3.json(queryUrl, function(faultdata) {
       
-      // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup("<h4>" + "Fault:"+ feature.properties.fault_name + "</h4> <hr> <h4>" + "Slip Rate:" + feature.properties.slip_rate + "</h4>");
+  
+   // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
+      earthquakesMarkers =  L.geoJSON(data.features, {
+         
+         pointToLayer: pointToLayer,
+         
+         // popup for each marker
+         onEachFeature:  addPopup
+         });
 
-    }
+      
+
+        FaultList = [];
+        faultdata.features.map(feature => {
+              
+            if(feature.properties.slip_rate === "Greater than 5.0 mm/yr"){
+              FaultList.push(feature);
+            }else if (feature.properties.slip_rate === "Between 1.0 and 5.0 mm/yr"){
+                FaultList.push(feature);
+            }
+        });
+        
+        faultMarkers = L.geoJson(FaultList, {
+          // Style each feature (in this case a neighborhood)
+          style: selectFaultStyle,
+          // Called on each feature
+          onEachFeature: function(feature, layer) {
+            
+            // Giving each feature a pop-up with information pertinent to it
+            layer.bindPopup("<h4>" + "Fault:"+ feature.properties.fault_name + "</h4> <hr> <h4>" + "Slip Rate:" + feature.properties.slip_rate + "</h4>");
+
+          }
+        });
+         createMaps(earthquakesMarkers,faultMarkers);
+        
+ 
   });
-  createMaps(faultMarkers);
+   
 });
-
-function createMaps(faults){
+function createMaps(earthquakes, faults){
 lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
@@ -65,6 +171,7 @@ lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{
     accessToken: API_KEY
   });
 
+  
   // Define a baseMaps object to hold our base layers
   const baseMaps = {
       "Light": lightmap,
@@ -72,7 +179,7 @@ lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{
   };
   // Create an overlay object
 const overlayMaps = {
-    //"Earthquakes": earthquakes,
+    "Earthquakes": earthquakes,
     "Faults": faults
   };
   
@@ -80,7 +187,7 @@ const overlayMaps = {
   const myMap = L.map("map", {
     center: [46.7557, -136.6825],
     zoom: 5,
-    layers: [darkmap, faults]
+    layers: [darkmap, earthquakes, faults]
   });
 
   // Create a layer control containing our baseMaps
@@ -88,5 +195,5 @@ const overlayMaps = {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
-  
+  createLegend(myMap);
 }
